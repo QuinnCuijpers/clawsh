cff067c969740899befd29325003b61bd4bf96e3
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-01-22 14:39:21 +0100

    "added support for quoted args"

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,14 @@ use anyhow::{Context, anyhow};
 use faccess::PathExt;
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{path::PathBuf, process::Command, str::FromStr};
+use std::{env::current_dir, path::PathBuf, process::Command, str::FromStr};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Builtin {
     Echo,
     Exit,
     Tipe,
+    Pwd,
 }
 
 impl FromStr for Builtin {
@@ -19,6 +20,7 @@ impl FromStr for Builtin {
             "echo" => Ok(Builtin::Echo),
             "exit" => Ok(Builtin::Exit),
             "type" => Ok(Builtin::Tipe),
+            "pwd" => Ok(Builtin::Pwd),
             _ => Err(anyhow!(format!("unknown builtin {s}"))),
         }
     }
@@ -30,20 +32,45 @@ fn main() -> anyhow::Result<()> {
         io::stdout().flush().context("flushing stdout")?;
         let mut buf = String::new();
         let _ = io::stdin().read_line(&mut buf).context("reading stdin")?;
-        let input = buf.trim_end();
-        let command_list: Vec<_> = input.split(" ").collect();
-        if let Ok(command) = Builtin::from_str(command_list[0]) {
+        let mut input = buf.trim_end();
+        let mut command_list = vec![];
+
+        while let Some((s1, mut s2)) = input.split_once(" ") {
+            if !s1.starts_with("\"") {
+                command_list.push(s1.to_string());
+                input = s2;
+                continue;
+            }
+            if let Some(idx) = s2.find("\"") {
+                let mut combined = s1.to_string();
+                combined.push_str(" ");
+                combined.push_str(&s2[..idx + 1]);
+                s2 = &s2[idx + 1..];
+                command_list.push(combined);
+                input = s2;
+            } else {
+                return Err(anyhow::anyhow!("unclosed qoutes for {s1}"));
+            }
+        }
+        // all input that can no longer be split on space is still added to the command list
+        if !input.is_empty() {
+            command_list.push(input.to_string());
+        }
+
+        println!("{:?}", command_list);
+        if let Ok(command) = Builtin::from_str(&command_list[0]) {
             match command {
                 Builtin::Echo => invoke_echo(&command_list[1..]),
                 Builtin::Exit => break,
                 Builtin::Tipe => invoke_type(&command_list[1..]),
+                Builtin::Pwd => invoke_pwd(&command_list[1..]),
             }
         } else {
             // TODO: add support for executing files
             let Some(env_path) = std::env::var_os("PATH") else {
                 panic!("PATH env var not set");
             };
-            let exec = command_list[0];
+            let exec = &command_list[0];
             if let Some(_) = find_exec_file(exec, env_path) {
                 let mut output = Command::new(exec).args(&command_list[1..]).spawn()?;
                 output.wait()?;
@@ -55,13 +82,13 @@ fn main() -> anyhow::Result<()> {
     anyhow::Ok(())
 }
 
-fn invoke_echo(cmd_list: &[&str]) {
+fn invoke_echo(cmd_list: &[String]) {
     let out = cmd_list.join(" ");
     println!("{out}");
 }
 
-fn invoke_type(cmd_list: &[&str]) {
-    for &cmd in cmd_list {
+fn invoke_type(cmd_list: &[String]) {
+    for cmd in cmd_list {
         if let Ok(_) = Builtin::from_str(cmd) {
             println!("{cmd} is a shell builtin");
             return;
@@ -70,7 +97,7 @@ fn invoke_type(cmd_list: &[&str]) {
         let Some(env_path) = std::env::var_os("PATH") else {
             panic!("PATH env var not set");
         };
-        if let Some(file_path) = find_exec_file(cmd, env_path) {
+        if let Some(file_path) = find_exec_file(&cmd, env_path) {
             println!("{cmd} is {}", file_path.display());
         } else {
             println!("{cmd}: not found");
@@ -96,4 +123,10 @@ fn find_exec_file(cmd: &str, env_path: std::ffi::OsString) -> Option<PathBuf> {
         }
     }
     None
+}
+
+fn invoke_pwd(_cmd_list: &[String]) {
+    if let Ok(curr) = current_dir() {
+        println!("{}", curr.display());
+    }
 }
