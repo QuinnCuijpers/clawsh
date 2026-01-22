4f0f1e001639fcb11628dfb1217d8323c0dd0e50
quinn cuijpers <quinn.cuijpers@gmail.com>
2026-01-22 01:24:28 +0100

    type command refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::str::FromStr;
+use std::{path::PathBuf, str::FromStr};
 
 use anyhow::{Context, anyhow};
 use faccess::PathExt;
@@ -56,31 +56,36 @@ fn invoke_type(cmd_list: &[&str]) {
     for cmd in cmd_list {
         if let Ok(_) = Builtin::from_str(cmd) {
             println!("{cmd} is a shell builtin");
+            return;
+        } 
+        // go through every directory and check if a file with the name exist that has exec permissions
+        let Some(env_path) = std::env::var_os("PATH") else {
+            panic!("PATH env var not set");
+        };
+        if let Some(file_path) = find_exec_file(cmd, env_path) {
+            println!("{cmd} is {}", file_path.display());
         } else {
-            // go through every directory and check if a file with the name exist that has exec permissions
-            let Some(env_path) = std::env::var_os("PATH") else {
-                panic!("PATH env var not set");
-            };
-            for path in std::env::split_paths(&env_path) {
-                if let Ok(exists) = path.try_exists() {
-                    if !exists {
-                        continue;
-                    }
-                    for dir in path.read_dir().expect("dir should exists") {
-                        if let Ok(dir) = dir {
-                            let file_name = dir.file_name();
-                            let file_path = dir.path();
-                            if file_name == *cmd && file_path.executable() {
-                                println!("{cmd} is {}", file_path.display());
-                                return;
-                            }
-                        }
-                    }
-                } else {
-                    continue;
-                }
-            }
             println!("{cmd}: not found");
         }
     }
+}
+
+fn find_exec_file(cmd: &&str, env_path: std::ffi::OsString) -> Option<PathBuf> {
+    for path in std::env::split_paths(&env_path) {
+        if let Ok(exists) = path.try_exists() {
+            if !exists {
+                continue;
+            }
+            for dir in path.read_dir().expect("dir should exist") {
+                if let Ok(dir) = dir {
+                    let file_name = dir.file_name();
+                    let file_path = dir.path();
+                    if file_name == *cmd && file_path.executable() {
+                        return Some(file_path);
+                    }
+                }
+            }
+        }
+    }
+    None
 }
